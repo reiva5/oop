027315472d83366b97c8d935b3ea275b2ad541a3
Amuzdalifa <a.muzdalifa@gmail.com>
2017-03-11 22:05:32 +0700

    json

--- map_generator.cpp
+++ map_generator.cpp
@@ -23,157 +23,117 @@ int main(){
 		{
 			{
 				{"Id",1},
-				{"Spesies","elang bondol"},
+				{"Spesies","Banteng"},
 				{"Massa", 1000},
 				{"Jinak", true},
 				{"Lokasi", {
-					{"x",1},
-					{"y",1}}
-				}
-			},
-			{
-				{"Id", 2},
-				{"Spesies", "elang bondol"},
-				{"Massa", 1200},
-				{"Jinak", true},
-				{"Lokasi", {
-					{"x",3},
+					{"x",15},
 					{"y",2}}
 				}
 			},
 			{
-				{"Id", 3},
-				{"Spesies", "elang bondol"},
-				{"Massa", 900},
+				{"Id",2},
+				{"Spesies","Banteng"},
+				{"Massa", 1000},
 				{"Jinak", true},
 				{"Lokasi", {
-					{"x",18},
+					{"x",5},
 					{"y",7}}
 				}
 			},
 			{
-				{"Id", 1},
-				{"Spesies", "hiu martil"},
-				{"Massa", 500000},
+				{"Id",1},
+				{"Spesies","Bekantan"},
+				{"Massa", 1000},
 				{"Jinak", true},
 				{"Lokasi", {
-					{"x",5},
-					{"y",0}}
+					{"x",17},
+					{"y",13}}
 				}
 			},
 			{
-				{"Id", 2},
-				{"Spesies", "hiu martil"},
-				{"Massa", 480000},
-				{"Jinak", false},
+				{"Id",2},
+				{"Spesies","Bekantan"},
+				{"Massa", 1000},
+				{"Jinak", true},
+				{"Lokasi", {
+					{"x",18},
+					{"y",13}}
+				}
+			},
+			{
+				{"Id",1},
+				{"Spesies","BadakCulaSatu"},
+				{"Massa", 1000},
+				{"Jinak", true},
 				{"Lokasi", {
 					{"x",10},
+					{"y",11}}
+				}
+			},
+			{
+				{"Id",2},
+				{"Spesies","BadakCulaSatu"},
+				{"Massa", 1000},
+				{"Jinak", true},
+				{"Lokasi", {
+					{"x",2},
 					{"y",14}}
 				}
 			},
 			{
-				{"Id", 3},
-				{"Spesies", "hiu martil"},
-				{"Massa", 300000},
-				{"Jinak", false},
+				{"Id",1},
+				{"Spesies","Buaya"},
+				{"Massa", 1000},
+				{"Jinak", true},
 				{"Lokasi", {
 					{"x",9},
-					{"y",16}}
-				}
-			},
-			{
-				{"Id", 1},
-				{"Spesies", "hiu karpet"},
-				{"Massa", 550000},
-				{"Jinak", true},
-				{"Lokasi", {
-					{"x",8},
-					{"y",1}}
-				}
-			},
-			{
-				{"Id", 2},
-				{"Spesies", "hiu karpet"},
-				{"Massa", 550000},
-				{"Jinak", true},
-				{"Lokasi", {
-					{"x",3},
-					{"y",5}}
-				}
-			},
-			{
-				{"Id", 1},
-				{"Spesies", "sting ray"},
-				{"Massa", 8000},
-				{"Jinak", true},
-				{"Lokasi", {
-					{"x",4},
 					{"y",3}}
 				}
 			},
 			{
-				{"Id", 2},
-				{"Spesies", "sting ray"},
-				{"Massa", 13000},
-				{"Jinak", true},
-				{"Lokasi", {
-					{"x",5},
-					{"y",10}}
-				}
-			},
-			{
-				{"Id", 3},
-				{"Spesies", "sting ray"},
-				{"Massa", 6000},
+				{"Id",2},
+				{"Spesies","Buaya"},
+				{"Massa", 1000},
 				{"Jinak", true},
 				{"Lokasi", {
 					{"x",10},
-					{"y",11}}
-				}
-			},
-			{
-				{"Id", 4},
-				{"Spesies", "sting ray"},
-				{"Massa", 5000},
-				{"Jinak", true},
-				{"Lokasi", {
-					{"x",3},
-					{"y",16}}
-				}
-			},
-			{
-				{"Id", 1},
-				{"Spesies", "manta ray"},
-				{"Massa", 5000},
-				{"Jinak", true},
-				{"Lokasi", {
-					{"x",0},
 					{"y",5}}
 				}
 			},
 			{
-				{"Id", 2},
-				{"Spesies", "manta ray"},
-				{"Massa", 5000},
+				{"Id",1},
+				{"Spesies","Rusa"},
+				{"Massa", 1000},
 				{"Jinak", true},
 				{"Lokasi", {
-					{"x",8},
+					{"x",9},
+					{"y",7}}
+				}
+			},
+			{
+				{"Id",2},
+				{"Spesies","Rusa"},
+				{"Massa", 1000},
+				{"Jinak", true},
+				{"Lokasi", {
+					{"x",3},
+					{"y",12}}
+				}
+			},
+			{
+				{"Id",1},
+				{"Spesies","Parkit"},
+				{"Massa", 1000},
+				{"Jinak", true},
+				{"Lokasi", {
+					{"x",3},
 					{"y",11}}
 				}
 			},
 			{
-				{"Id", 3},
-				{"Spesies", "manta ray"},
-				{"Massa", 5000},
-				{"Jinak", true},
-				{"Lokasi", {
-					{"x",1},
-					{"y",16}}
-				}
-			},
-			{
 				{"Id", 1},
-				{"Spesies", "garuda"},
+				{"Spesies", "Garuda"},
 				{"Massa", 5000},
 				{"Jinak", false},
 				{"Lokasi", {
@@ -183,7 +143,7 @@ int main(){
 			},
 			{
 				{"Id", 2},
-				{"Spesies", "garuda"},
+				{"Spesies", "Garuda"},
 				{"Massa", 5000},
 				{"Jinak", true},
 				{"Lokasi", {
@@ -193,7 +153,7 @@ int main(){
 			},
 			{
 				{"Id", 3},
-				{"Spesies", "garuda"},
+				{"Spesies", "Garuda"},
 				{"Massa", 5000},
 				{"Jinak", true},
 				{"Lokasi", {
@@ -201,6 +161,176 @@ int main(){
 					{"y",5}}
 				}
 			},
+			{
+				{"Id", 1},
+				{"Spesies", "HarimauSumatra"},
+				{"Massa", 500000},
+				{"Jinak", true},
+				{"Lokasi", {
+					{"x",17},
+					{"y",2}}
+				}
+			},
+			{
+				{"Id", 2},
+				{"Spesies", "HarimauSumatra"},
+				{"Massa", 500000},
+				{"Jinak", true},
+				{"Lokasi", {
+					{"x",18},
+					{"y",2}}
+				}
+			},
+			{
+				{"Id", 3},
+				{"Spesies", "HarimauSumatra"},
+				{"Massa", 500000},
+				{"Jinak", true},
+				{"Lokasi", {
+					{"x",5},
+					{"y",14}}
+				}
+			},
+			{
+				{"Id", 1},
+				{"Spesies", "HiuMartil"},
+				{"Massa", 500000},
+				{"Jinak", true},
+				{"Lokasi", {
+					{"x",5},
+					{"y",0}}
+				}
+			},
+			{
+				{"Id", 2},
+				{"Spesies", "HiuMartil"},
+				{"Massa", 480000},
+				{"Jinak", false},
+				{"Lokasi", {
+					{"x",10},
+					{"y",14}}
+				}
+			},
+			{
+				{"Id", 3},
+				{"Spesies", "HiuMartil"},
+				{"Massa", 300000},
+				{"Jinak", false},
+				{"Lokasi", {
+					{"x",9},
+					{"y",16}}
+				}
+			},
+			{
+				{"Id", 1},
+				{"Spesies", "HiuKarpet"},
+				{"Massa", 550000},
+				{"Jinak", true},
+				{"Lokasi", {
+					{"x",8},
+					{"y",1}}
+				}
+			},
+			{
+				{"Id", 2},
+				{"Spesies", "HiuKarpet"},
+				{"Massa", 550000},
+				{"Jinak", true},
+				{"Lokasi", {
+					{"x",3},
+					{"y",5}}
+				}
+			},
+			{
+				{"Id", 1},
+				{"Spesies", "StingRay"},
+				{"Massa", 8000},
+				{"Jinak", true},
+				{"Lokasi", {
+					{"x",4},
+					{"y",3}}
+				}
+			},
+			{
+				{"Id", 2},
+				{"Spesies", "StingRay"},
+				{"Massa", 13000},
+				{"Jinak", true},
+				{"Lokasi", {
+					{"x",5},
+					{"y",10}}
+				}
+			},
+			{
+				{"Id", 3},
+				{"Spesies", "StingRay"},
+				{"Massa", 6000},
+				{"Jinak", true},
+				{"Lokasi", {
+					{"x",10},
+					{"y",11}}
+				}
+			},
+			{
+				{"Id", 4},
+				{"Spesies", "StingRay"},
+				{"Massa", 5000},
+				{"Jinak", true},
+				{"Lokasi", {
+					{"x",3},
+					{"y",16}}
+				}
+			},
+			{
+				{"Id", 1},
+				{"Spesies", "MantaRay"},
+				{"Massa", 5000},
+				{"Jinak", true},
+				{"Lokasi", {
+					{"x",0},
+					{"y",5}}
+				}
+			},
+			{
+				{"Id", 2},
+				{"Spesies", "MantaRay"},
+				{"Massa", 5000},
+				{"Jinak", true},
+				{"Lokasi", {
+					{"x",8},
+					{"y",11}}
+				}
+			},
+			{
+				{"Id", 3},
+				{"Spesies", "MantaRay"},
+				{"Massa", 5000},
+				{"Jinak", true},
+				{"Lokasi", {
+					{"x",1},
+					{"y",16}}
+				}
+			},
+			{
+				{"Id",1},
+				{"Spesies","Elang"},
+				{"Massa", 1000},
+				{"Jinak", true},
+				{"Lokasi", {
+					{"x",1},
+					{"y",1}}
+				}
+			},
+			{
+				{"Id", 2},
+				{"Spesies", "Elang"},
+				{"Massa", 1200},
+				{"Jinak", true},
+				{"Lokasi", {
+					{"x",3},
+					{"y",2}}
+				}
+			}
 		}
 		);
 
